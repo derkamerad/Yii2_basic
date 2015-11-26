e577592a7054763ad8b2d44837c1046dda15f938
Kamerad <der.kamerad@mail.ru>
2015-11-26 18:12:56 +0700

    cookieValidationKey

--- config/web.php
+++ config/web.php
@@ -9,7 +9,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'gsdg5235325@#%@%',
+            'cookieValidationKey' => 'gsdg5235325hdhaiu',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
